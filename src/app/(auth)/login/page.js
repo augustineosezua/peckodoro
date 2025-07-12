8f1b06af0e287c0ee21a88f21052e81e66e8972d
Augustine <augustineosezua1@gmail.com>
2025-07-12 18:48:39 -0400

    v0.1.202 prod login in issues

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -100,6 +100,7 @@ export default function LoginPage() {
                   id: "signing-up",
                 }
               );
+              console.log(ctx)
             },
           }
         );
