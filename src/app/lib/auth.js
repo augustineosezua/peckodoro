9faed1c65d509f960428a6c8be341935c2e04456
chickenjsx <augustineosezua1@gmail.com>
2025-07-15 15:09:36 -0400

    we'll figure it out

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -55,9 +55,7 @@ export const auth = betterAuth({
     spotify: {
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      scope: [
-        "user-read-email user-read-private user-read-playback-state user-modify-playback-state streaming",
-      ],
+      scope: ["user-read-email user-read-private user-read-playback-state user-modify-playback-state streaming"],
     },
   },
   trustedOrigins: [
@@ -100,19 +98,14 @@ export const auth = betterAuth({
     account: {
       create: {
         before(account, context) {
+          console.log(account);
           const withEncryptedTokens = { ...account };
           if (account.accessToken) {
-            const encryptedAccessToken = encrypt(
-              account.accessToken,
-              process.env.ENCRYPTION_KEY
-            );
+            const encryptedAccessToken = encrypt(account.accessToken, process.env.ENCRYPTION_KEY);
             withEncryptedTokens.accessToken = encryptedAccessToken;
           }
           if (account.refreshToken) {
-            const encryptedRefreshToken = encrypt(
-              account.refreshToken,
-              process.env.ENCRYPTION_KEY
-            );
+            const encryptedRefreshToken = encrypt(account.refreshToken, process.env.ENCRYPTION_KEY);
             withEncryptedTokens.refreshToken = encryptedRefreshToken;
           }
           return {
